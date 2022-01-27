3aad0cc7c52ad9d36223c5848d29b6111453f134
Michael-Musili <93195066+Michael-Musili@users.noreply.github.com>
2022-01-27 14:28:14 +0300

    added functionality.

--- index.js
+++ index.js
@@ -15,18 +15,23 @@ function increment(){
 
 
 }
+
 // 1. Create a function, save(), which logs out the count when it's called
 let saveD = document.getElementById("save-btn")
 function save(){
     countEl.innerText=count
 }
 
+
 function saveB() {
     // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
     let saveEl = document.getElementById("save-el")
     countstr = count + " - " 
+
     // 3. Render the variable in the saveEl using innerText
     saveEl.textContent+= countstr
+    countEl.innerText=0
+    count =0
     // NB: Make sure to not delete the existing content of the paragraph
     console.log(count)
 }
